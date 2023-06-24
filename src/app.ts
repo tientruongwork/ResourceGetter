e0f351f62c3b992276bba2dfe842f05c6aeb9702
Tien Truong <tientruong.work@gmail.com>
2023-06-24 22:59:06 +0700

    [Tien] expose html file

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,27 @@ import { youtubeRoute } from "./routes/youtubeDownloadRoute";
 import { pinterestRoute } from "./routes/pinterestDownloadRoute";
 import bodyParser from "body-parser";
 import cors from "cors";
+import path from "path";
 
 const app: Express = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    exposedHeaders: ["Content-Disposition"],
+  })
+);
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 
+app.use(express.static(path.join(__dirname, "../public")));
+
 app.use("/youtube", youtubeRoute);
 app.use("/pinterest", pinterestRoute);
+
+app.get("/", (_, response) => {
+  response.render("../public/index.html");
+});
 
 app.listen(PORT, () => {
   console.log(`Application is running on port:${PORT}`);
